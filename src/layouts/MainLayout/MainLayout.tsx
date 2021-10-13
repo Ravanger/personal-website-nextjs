242f977259255763ed09b29a6e9967ab1e9a7194
Ravanger <Boris.Rozzy@gmail.com>
2021-10-13 16:40:03 -0400

    Position shenanigans

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,12 +1,10 @@
 import styles from "./MainLayout.module.scss"
 import TopNav from "components/Layout/TopNav"
 import BottomNav from "components/Layout/BottomNav"
-import Spacer from "components/common/Spacer"
 
 const MainLayout: React.FC = ({ children }) => {
   return (
     <div className={styles.layout}>
-      <Spacer size="0.3rem" />
       <TopNav />
       {children}
       <BottomNav />
