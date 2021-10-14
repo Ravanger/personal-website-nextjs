4bb0bebcda66bb5d2cb437c8ff778c02e046fd60
Ravanger <Boris.Rozzy@gmail.com>
2021-10-13 22:08:53 -0400

    Topnav padding shenanigans

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,11 +1,13 @@
 import styles from "./MainLayout.module.scss"
 import TopNav from "components/Layout/TopNav"
 import BottomNav from "components/Layout/BottomNav"
+import Spacer from "components/common/Spacer"
 
 const MainLayout: React.FC = ({ children }) => {
   return (
     <div className={styles.layout}>
       <TopNav />
+      <Spacer size="6rem" />
       {children}
       <BottomNav />
     </div>
