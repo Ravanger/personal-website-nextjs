eca92c324da1591c03357eee668efb0f792f9071
Ravanger <Boris.Rozzy@gmail.com>
2021-10-14 02:19:16 -0400

    Topnav logo link back to home page

--- src/components/Layout/TopNav/TopNav.tsx
+++ src/components/Layout/TopNav/TopNav.tsx
@@ -2,19 +2,22 @@ import styles from "./TopNav.module.scss"
 import { BsMoon } from "react-icons/bs"
 import Image from "next/image"
 import ButtonLink from "components/common/ButtonLink"
+import Link from "next/link"
 
 const TopNav: React.FC = () => {
   return (
     <header className={styles.header}>
-      <button className={styles.logo}>
-        <Image
-          src="/images/logo/dark_solid.svg"
-          alt="Logo"
-          width="64"
-          height="64"
-          layout="responsive"
-        />
-      </button>
+      <Link href="/">
+        <a className={styles.logo}>
+          <Image
+            src="/images/logo/dark_solid.svg"
+            alt="Logo"
+            width="64"
+            height="64"
+            layout="responsive"
+          />
+        </a>
+      </Link>
       <button className={styles.themeSwitch}>
         <BsMoon />
       </button>
