95cc8c005f5181199f9b08353a313c722dbd2c56
Ravanger <Boris.Rozzy@gmail.com>
2021-11-01 20:14:25 -0400

    Package upgrades

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 
+const withPlugins = require("next-compose-plugins")
 const { withPlaiceholder } = require("@plaiceholder/next")
 
-module.exports = withPlaiceholder({
-  reactStrictMode: true,
-})
+module.exports = withPlugins([[withPlaiceholder, { reactStrictMode: true }]])
